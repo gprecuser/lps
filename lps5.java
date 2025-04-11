9d94b782044b14f3d6e8971d03472a40379f793b
gprecuser <gprecbot3@gmail.com>
2025-04-11 23:34:37 +0530

    Update lps5.java

--- lps5.java
+++ lps5.java
@@ -1,149 +1,112 @@
-import java.util.*; 
-import java.io.*; 
-class Main 
-{ 
-static char ntermnl[],termnl[]; 
-static int ntlen,tlen; 
-static String grmr[][],fst[],flw[]; 
-public static void main(String args[]) throws IOException 
-{ 
-String nt,t; 
-int i,j,n; 
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
-System.out.println("Enter the non-terminals"); 
-nt=br.readLine(); 
-ntlen=nt.length(); 
-ntermnl=new char[ntlen]; 
-ntermnl=nt.toCharArray(); 
-System.out.println("Enter the terminals"); 
-t=br.readLine(); 
-tlen=t.length(); 
-termnl=new char[tlen]; 
-termnl=t.toCharArray(); 
-System.out.println("Specify the grammar(Enter 9 for epsilon production)"); 
-grmr=new String[ntlen][]; 
-for(i=0;i<ntlen;i++) 
-{ 
-System.out.println("Enter the number of productions for "+ntermnl[i]); 
-n=Integer.parseInt(br.readLine()); 
-grmr[i]=new String[n]; 
-System.out.println("Enter the productions"); 
-for(j=0;j<n;j++) 
-grmr[i][j]=br.readLine(); 
-} 
-fst=new String[ntlen]; 
-for(i=0;i<ntlen;i++) 
-fst[i]=first(i); 
-System.out.println("First Set"); 
-for(i=0;i<ntlen;i++) 
-System.out.println(removeDuplicates(fst[i])); 
-flw=new String[ntlen]; 
-for(i=0;i<ntlen;i++) 
-flw[i]=follow(i); 
-System.out.println("Follow Set"); 
-for(i=0;i<ntlen;i++) 
-System.out.println(removeDuplicates(flw[i])); 
-} 
-static String first(int i) 
-{ 
-int j,k,l=0,found=0; 
-String temp="",str=""; 
-for(j=0;j<grmr[i].length;j++) 
-{ 
-for(k=0;k<grmr[i][j].length();k++,found=0) 
-{ 
-for(l=0;l<ntlen;l++)   
-{ 
-if(grmr[i][j].charAt(k)==ntermnl[l]) 
-{ 
-str=first(l); 
-if(!(str.length()==1 && str.charAt(0)=='9')) 
-temp=temp+str; 
-found=1; 
-break; 
-} 
-} 
-if(found==1) 
-{ 
-if(str.contains("9")) 
-continue; 
-} 
-else   
-temp=temp+grmr[i][j].charAt(k); 
-break; 
-} 
-} 
-return temp; 
-} 
-static String follow(int i) 
-{ 
-char pro[],chr[]; 
-String temp=" "; 
-int j,k,l,m,n,found=0; 
-if(i==0) 
-temp="$"; 
-for(j=0;j<ntlen;j++) 
-{ 
-for(k=0;k<grmr[j].length;k++)   
-{ 
-pro=new char[grmr[j][k].length()]; 
-pro=grmr[j][k].toCharArray(); 
-for(l=0;l<pro.length;l++) 
-{ 
-if(pro[l]==ntermnl[i]) 
-{ 
-if(l==pro.length-1) 
-{ 
-if(j<i) 
-temp=temp+flw[j]; 
-} 
-else 
-{ 
-for(m=0;m<ntlen;m++) 
-{ 
-if(pro[l+1]==ntermnl[m]) 
-{ 
-chr=new char[fst[m].length()]; 
-chr=fst[m].toCharArray(); 
-for(n=0;n<chr.length;n++) 
-{ 
-if(chr[n]=='9')   
-{ 
-if(l+1==pro.length-1) 
-temp=temp+follow(j);   
-else 
-temp=temp+follow(m); 
-} 
-else 
-temp=temp+chr[n];   
-} 
-found=1; 
-} 
-} 
-if(found!=1) 
-temp=temp+pro[l+1]; 
-} 
-} 
-} 
-} 
-} 
-return temp; 
-} 
-static String removeDuplicates(String str) 
-{ 
-int i; 
-char ch; 
-boolean seen[] = new boolean[256]; 
-StringBuilder sb = new StringBuilder(seen.length); 
-for(i=0;i<str.length();i++) 
-{ 
-ch=str.charAt(i); 
-if (!seen[ch]) 
-{ 
-seen[ch] = true; 
-sb.append(ch); 
-} 
-} 
-return sb.toString(); 
-} 
+import java.util.*;
+
+public class Main {
+    static String[] first;
+    static String[] follow; 
+    static String nt;
+    
+    public static String removeDuplicate(String str) {
+       String s="";
+        for (int i=0;i<str.length();i++) {
+            if (s.indexOf(str.charAt(i))==-1) {
+                s+=str.charAt(i);
+            }
+        }
+        return s;
+    }
+    
+    static void getFollow(String lhs, String rhs){
+        for (int j = 0; j < rhs.length(); j++) {
+            char ch = rhs.charAt(j);
+            if(ch<'A' || ch>'Z') continue;
+            int idx = nt.indexOf(ch+"");
+            if (j == rhs.length() - 1 || rhs.charAt(j+1)=='/') {
+                follow[idx] += follow[nt.indexOf(lhs)];
+                continue;
+            }
+            
+            for (int k = j + 1; k < rhs.length(); k++) {
+                char nch = rhs.charAt(k);
+                if(nch=='/'){
+                    follow[idx] += follow[nt.indexOf(lhs)];
+                    break;
+                }
+                if (nch < 'A' || nch > 'Z') {
+                    follow[idx] += nch;
+                    break;
+                }
+                follow[idx] += first[nt.indexOf(nch+"")];
+                if (follow[idx].contains("@")) {
+                    follow[idx] = follow[idx].replace("@", "");
+                    if(k==rhs.length()-1) 
+                        follow[idx] += follow[nt.indexOf(lhs)];
+                }
+                else break;
+                
+            }
+        }
+    }
+    
+    static String getFirst(String s) {
+        String[] rules = s.split("|");
+        String t="";
+        for (String r : rules) {
+            for(int k=0;k<r.length();k++){
+                char ch = r.charAt(k);
+                int idx=nt.indexOf(ch+"");
+                if (ch < 'A' || ch > 'Z') {
+                    t+=ch;
+                    break;
+                }
+                if(k<r.length()-1 && first[idx].contains("@")) {
+                    t+=first[idx].replace("@", "");
+                }
+                else{
+                    t+=first[idx];
+                    break;
+                }
+            }
+        }
+        return t;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("No. of productions: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        System.out.print("Non-Terminals: ");
+        nt = sc.nextLine();
+        
+        first=new String[n];
+        follow=new String[n];
+        String[][] grmr = new String[n][2];
+        System.out.println("Grammar: ");
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String[] p = s.split("->");
+            grmr[i][0] = p[0];
+            grmr[i][1] = p[1];
+            follow[i]="";
+            first[i]="";
+        }
+
+        System.out.println("FIRST : ");
+        for (int i = n - 1; i >= 0; i--) {
+            first[i]= removeDuplicate(getFirst(grmr[i][1]));
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(nt.charAt(i) + " -> " + first[i]);
+        }
+        
+        System.out.println("FOLLOW : ");
+        follow[0]="$";
+        for(int i=0;i<n;i++)
+            getFollow(grmr[i][0], grmr[i][1]);
+        for (int i = 0; i < n; i++) {
+            String res= removeDuplicate(follow[i]);
+            System.out.println(nt.charAt(i) + " -> " + res);
+        }
+    }
 }
